3a88a76b28fd83a527c78b21d6dfabe0bd49d5e2
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2018-11-22 21:59:50 +0100

    [YE-0] Deploy with travis.

--- deploy-maven-central.sh
+++ deploy-maven-central.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-openssl version -a 
-openssl aes-256-cbc -pass pass:$OPENSSL_PWD -in private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s settings.xml org.jacoco:jacoco-maven-plugin:prepare-agent clean deploy sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=$SONAR_ORGANIZATION -Dsonar.login=$SONAR
+echo "Building $BRANCH branch"
+
+if [ "$BRANCH" = "develop" ]; then
+  openssl version -a
+  openssl aes-256-cbc -pass pass:$OPENSSL_PWD -in private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s settings.xml org.jacoco:jacoco-maven-plugin:prepare-agent clean deploy sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=$SONAR_ORGANIZATION -Dsonar.login=$SONAR
+elif [ "$BRANCH" = "master" ]; then
+  openssl version -a
+  openssl aes-256-cbc -pass pass:$OPENSSL_PWD -in private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s settings.xml clean deploy
+else
+  mvn -V -s settings.xml clean package
+fi
