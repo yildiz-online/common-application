ee36f254dabd5f51ccf038d6434b6e02b8f5b178
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-01-05 16:48:05 +0100

    [YE-0] Support private repo for faster releases.

--- deploy-maven-central.sh
+++ deploy-maven-central.sh
@@ -8,6 +8,7 @@ if [ "$BRANCH" = "develop" ]; then
 elif [ "$BRANCH" = "master" ]; then
   openssl version -a
   openssl aes-256-cbc -pass pass:$OPENSSL_PWD -in private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s settings.xml clean deploy
+  mvn -V -s settings.xml deploy -Dmaven.plugin.nexus.skip
 else
   mvn -V -s settings.xml clean package
 fi
