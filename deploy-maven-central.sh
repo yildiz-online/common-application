756630a1b14d57e59242d8102e18cfd4409d9c2c
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-05-12 13:46:29 +0200

    [FEATURE] Use Vault.

--- deploy-maven-central.sh
+++ deploy-maven-central.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+SECRETS=$(curl -sS -H "X-Vault-Token: $VAULT_TOKEN" -X GET https://vault.yildiz-games.be/v1/kv/yildiz-engine)
+
+#Some variables need to be exported as env variable to be used by external processes.
+
+export GH_TOKEN=$(echo $SECRETS | jq -r '.data.GH_TOKEN')
+OPENSSL_PWD=$(echo $SECRETS | jq -r '.data.OPENSSL_PWD')
+export GPG_KEY=$(echo $SECRETS | jq -r '.data.GPG_KEY')
+export GPG_PWD=$(echo $SECRETS | jq -r '.data.GPG_PWD')
+export OSSRH_USER_TOKEN=$(echo $SECRETS | jq -r '.data.OSSRH_USER_TOKEN')
+export OSSRH_PWD_TOKEN=$(echo $SECRETS | jq -r '.data.OSSRH_PWD_TOKEN')
+export REPO_USER=$(echo $SECRETS | jq -r '.data.REPO_USER')
+export REPO_PASSWORD=$(echo $SECRETS | jq -r '.data.REPO_PASSWORD')
+SONAR=$(echo $SECRETS | jq -r '.data.SONAR')
+SONAR_ORGANIZATION=$(echo $SECRETS | jq -r '.data.SONAR_ORGANIZATION')
+
 echo "Building $BRANCH branch"
 
 if [ "$BRANCH" = "develop" ]; then
